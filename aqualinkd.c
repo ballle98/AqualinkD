1a9311af30d5b5a4665ce23c16af97dfb465350f
Lee Ballard <ballle98@gmail.com>
2019-07-31 22:07:08 -0500

    ballle98/AqualinkD#33: sleep with websocket active

--- aqualinkd.c
+++ aqualinkd.c
@@ -1540,11 +1540,10 @@ void main_loop()
     _aqualink_data.swg_percent = 0;
     _aqualink_data.swg_ppm = 0;
   }
+  memset(&_aqualink_data.last_active_time, 0, sizeof(struct timespec));
 
   pthread_mutex_init(&_aqualink_data.mutex, NULL);
   pthread_cond_init(&_aqualink_data.thread_finished_cond, NULL);
-
-  clock_gettime(CLOCK_REALTIME, &_aqualink_data.last_active_time);
 
   if (!start_net_services(&mgr, &_aqualink_data))
   {
